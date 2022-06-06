ba67fecbd73912ecbb706199d178dd089ca124f2
Đạt.Code <73332200+datyasuo1@users.noreply.github.com>
2022-06-06 13:52:26 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello T2010A with React.js (Dat yasuo)
         </p>
         <a
           className="App-link"
